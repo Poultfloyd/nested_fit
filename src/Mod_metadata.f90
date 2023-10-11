6a3b584998ae50d6189519393a59d03076a94d3d
poultfloyd <poultfloyd@gmail.com>
2023-10-11 15:11:11 +0200

    Fixed non compiling issues with MPI options

--- src/Mod_metadata.f90
+++ src/Mod_metadata.f90
@@ -1,20 +1,16 @@
 MODULE MOD_METADATA
-  ! Module for program runtime metadata
-
   IMPLICIT NONE
-  CHARACTER(LEN=5), PARAMETER :: version_full = '4.5.2'
+  ! Module for program runtime metadata
+  
+  CHARACTER(LEN=5), PARAMETER :: version_full = '4.5.5'
   CHARACTER(LEN=3), PARAMETER :: version = '4.5'
-  CHARACTER(LEN=18), PARAMETER :: nf_child_proc_name = 'nf_child_proc4.5.2'
-
-  LOGICAL, PARAMETER :: parallel_on = .FALSE.
-  LOGICAL, PARAMETER :: parallel_mpi_on = .FALSE.
-  LOGICAL, PARAMETER :: static_seed = .FALSE.
-
+  CHARACTER(LEN=18), PARAMETER :: nf_child_proc_name = 'nf_child_proc4.5.5'
+  
 #ifdef FUNC_TARGET
-  CHARACTER(LEN=20), PARAMETER :: exec_target_name = 'nested_fit_func4.5.2'
+  CHARACTER(LEN=20), PARAMETER :: exec_target_name = 'nested_fit_func4.5.5'
 #else
-  CHARACTER(LEN=15), PARAMETER :: exec_target_name = 'nested_fit4.5.2'
+  CHARACTER(LEN=15), PARAMETER :: exec_target_name = 'nested_fit4.5.5'
 #endif
-
-
+  
+  
 END MODULE MOD_METADATA
